4f632d388bc0d45b4bd43628d6828f36a0eec1fe
Youmna-Mosadak <youmna.mosadak@gmail.com>
2023-08-10 14:41:24 -0700

    C - Hello, World tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - the entry point
+ *
+ * description - a C program prints with put function
+ *
+ * return - zero(successed)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
